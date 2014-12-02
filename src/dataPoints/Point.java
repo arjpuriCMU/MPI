d2f34be7ee623510acd045277eba9e4f0c74334f
Arjun Puri <arjpuri@andrew.cmu.edu>
2014-12-02 02:18:41 -0500

    basic point class

---
+++ src/dataPoints/Point.java
@@ -0,0 +1,50 @@
+package dataPoints;
+
+import java.util.List;
+
+public class Point {
+	public Double x;
+	public Double y;
+	
+	public Point(Double x, Double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if (!(o instanceof Point)){
+			return false;
+		}
+		Point new_point = (Point) o;
+		if (this.x.equals(new_point.x) || this.y.equals(new_point.y)){
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public int hashCode(){
+		Double x = new Double(this.x);
+		Double y = new Double(this.y);
+		return x.hashCode() + y.hashCode();
+	}
+	
+	public Double distance(Point p){
+		Double val1 = Math.pow(x - p.x,2);
+		Double val2 = Math.pow(y-p.y, 2);
+		return Math.sqrt(val1+val2);
+	}
+	
+	public static Point mean(List<Point> points){
+		Double sum_x = 0.0;
+		Double sum_y = 0.0;
+		for (Point p : points){
+			sum_x += p.x;
+			sum_y += p.y;
+		}
+		return new Point(sum_x/points.size(), sum_y/points.size());
+	}
+	
+	
+}
