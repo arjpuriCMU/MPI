e7f0663cdfda64a6821b32ff70bd85f77d3aeaac
Karan Sharma <karansharma2727@gmail.com>
2014-12-03 04:04:26 -0500

    Completed DNA Sequential and DNA strand scipt

---
+++ src/DNA/SequentialDNACluster.java
@@ -0,0 +1,97 @@
+package DNA;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class SequentialDNACluster {
+
+	public static void main(String[] args){
+		if (args.length != 2){
+			System.out.println("There must be 2 arguments in this order: k, file. Where k is the number of clusters, and file is the .csv of dna strands");
+			return;
+		}
+		Integer k = null;
+		String file_path = null;
+		
+		/*Retrieve the number of cluster points, and the file path of all the .csv */
+		try{
+			k = Integer.valueOf(args[0]);
+			file_path = args[1];
+		}
+		catch (Exception e){
+			System.out.println("Please input correct arguments");
+		}
+		BufferedReader br = null;
+		String line;
+		List<Strand> strands = new ArrayList<Strand>();
+		String[] temp_point;
+		
+		/*Parse the .csv file and converts all data to the Point abstraction we have defined */
+		try {
+			br = new BufferedReader(new FileReader(file_path));
+			while ( (line = br.readLine()) != null){
+				strands.add(new Strand(line));
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Incorrect file path!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		/*Pick any k random centroids from the given data */
+		Strand[] centroids = randomCentroids(strands,k);
+		/*Time the execution */
+		long startTime = System.currentTimeMillis();
+		Strand[] results = sequentialKMeans(strands, centroids, k);
+		long stopTime = System.currentTimeMillis();
+	    long elapsedTime = stopTime - startTime;
+	}
+	
+	public static Strand[] sequentialKMeans(List<Strand> points, Strand[] centroids,int k){
+		List<List<Strand>> clusters = new ArrayList<List<Strand>>();
+		/*Create k clusters- one for each centroid */
+		for (int i = 0; i < k; i++){
+			clusters.add(new ArrayList<Strand>());
+		}
+		while(true){
+			/*For all points, find the centroid closest to it, and add that point to that cluster. */
+			for (Strand p : points){
+				int index = p.closetPointIndex(centroids);
+				clusters.get(index).add(p);
+			}
+			Strand[] new_centroids = new Strand[k];
+			int count = 0;
+			
+			/*Re calculate all the centroids by determining the mean of all the clusters */
+			for (List<Strand> list : clusters){
+				new_centroids[count] = Strand.getCentroid(list);
+			}
+			
+			/*If there are no changes to the centroids we are done */
+			if (Arrays.equals(centroids, new_centroids)){
+				return new_centroids;
+			}
+			centroids = Arrays.copyOf(new_centroids, new_centroids.length);
+		}
+	}
+
+	/*From a list of points get a list of n random points */
+	public static Strand[] randomCentroids(List<Strand> points, int n){
+		List<Strand> linked_list_points = new LinkedList<Strand>(points);
+		Collections.shuffle(linked_list_points);
+		return (Strand[]) linked_list_points.subList(0, n).toArray();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/DNA/Strand.java
@@ -0,0 +1,126 @@
+package DNA;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Strand {
+	public String strand_string;
+
+	public Strand(String strand){
+		this.strand_string = strand;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if (!(o instanceof Strand)){
+			return false;
+		}
+		Strand new_strand = (Strand) o;
+		if (this.strand_string.equals(new_strand.strand_string)){
+			return false;
+		}
+		return true;
+	}
+	
+
+    @Override
+	public int hashCode(){
+		return strand_string.hashCode();
+	}
+
+
+	public int similarity(Strand s){
+        int count = 0;
+        for(int i = 0; i < strand_string.length(); i++)
+        {
+            if(strand_string.charAt(i) == s.strand_string.charAt(i))
+                count++;
+        }
+
+		return count;
+	}
+	
+	public static Strand getCentroid(List<Strand> strands)
+    {
+        int strand_length = strands.get(0).strand_string.length();
+        int[][] bases = new int[strand_length][4];
+        //Stores counts for each index: A:0 G:1 C:2 T:3
+		for (Strand s : strands){
+            for(int i = 0; i < strand_length; i++)
+            {
+                switch (s.strand_string.charAt(i))
+                {
+                    case 'A':
+                        bases[i][0] =+ 1;
+                        break;
+                    case 'G':
+                        bases[i][1] =+ 1;
+                        break;
+                    case 'C':
+                        bases[i][2] =+ 1;
+                        break;
+                    case 'T':
+                        bases[i][3] =+ 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+		}
+
+        char[] centroidChars = new char[strand_length];
+
+        // Store most frequent character for each index
+        for(int i = 0; i < strand_length; i++)
+        {
+            int maxIndex = 0;
+            int maxVal = 0;
+            for (int j = 0; j < 4; j++)
+            {
+                if(bases[i][j] > maxVal)
+                {
+                    maxVal = bases[i][j];
+                    maxIndex = j;
+                }
+
+            }
+
+            switch (maxIndex)
+            {
+                case 0:
+                    centroidChars[i] = 'A';
+                    break;
+                case 1:
+                    centroidChars[i] = 'G';
+                    break;
+                case 2:
+                    centroidChars[i] = 'C';
+                    break;
+                case 3:
+                    centroidChars[i] = 'T';
+                    break;
+            }
+        }
+		return new Strand(centroidChars.toString());
+	}
+
+	
+	public int closetPointIndex(Strand[] centroids) {
+		int temp_similarity;
+		int max_similarity = 0;
+		int close_index = 0;
+		Strand p;
+		for (int i = 0; i < centroids.length; i++){
+			p = centroids[i];
+			temp_similarity = this.similarity(p);
+			if (temp_similarity > max_similarity){
+				max_similarity = temp_similarity;
+				close_index = i;
+			}
+		}
+		return close_index;
+	}
+	
+}
